3ba68cc40348fcf5c9bd09d04b7b4277ef22cf8d
shrenky <shrenkyang@gmail.com>
2019-06-08 22:36:40 +0800

    Create Demo Project

---
+++ src/webparts/demoCi/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDemoCiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DemoCiWebPartStrings' {
+  const strings: IDemoCiWebPartStrings;
+  export = strings;
+}
